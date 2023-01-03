b6b5726813e7f3c82643598efed0fc14485fdb71
kasem32833 <abulkasem32833@gmail.com>
2023-01-03 14:01:23 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    
+    <div class="bg-black h-screen text-green-500 flex items-center justify-center">
+        <div class=" text-center p-32 border rounded-md border-gray-300">
+            <h1 class="text-3xl font-bold" >
+            Welcome to <span class="text-red-500">46</span> -express bd ltd.com
+            </h1>
+            <h3 class="text-gray-300">We are Commited to your Safe Journy</h3>
+        </div>
+    </div>
+</template>
