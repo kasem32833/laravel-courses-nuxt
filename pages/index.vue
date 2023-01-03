fd268be036ba0801940759889578d468277243f2
kasem32833 <abulkasem32833@gmail.com>
2023-01-03 14:07:29 +0300

    text change

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,9 @@
     <div class="bg-black h-screen text-green-500 flex items-center justify-center">
         <div class=" text-center p-32 border rounded-md border-gray-300">
             <h1 class="text-3xl font-bold" >
-            Welcome to <span class="text-red-500">46</span> -express bd ltd.com
+            Welcome to <span class="text-red-500">Mother</span> Kindergarten
             </h1>
-            <h3 class="text-gray-300">We are Commited to your Safe Journy</h3>
+            <h3 class="text-gray-300">We are comitted to quality education</h3>
         </div>
     </div>
 </template>
